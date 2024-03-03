9cc92779f6ad1fa41fa364529223548ae615b705
panvp <vaibhavpandey7799@gmail.com>
2024-03-04 00:22:16 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+</script>
+
+<template>
+<div class="header">
+    <div class="headerTitle">
+      League Web UI
+    </div>
+    <div class="navLinks">
+        <div class="scheduleLink"> Schedule</div>
+        <div> Leaderboard</div>
+    </div>
+  </div>
+  <HomeView/>
+
+  <div class="footer">
+    <div class="footerItem">
+      API Version: 1.0
+    </div>
+  </div>
+</template>
+<style scoped>
+.header{
+  background-color: #025FEB;
+  height: 60px;
+  font-size: 1.5rem;
+  color: #FFFFFF;
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.footer{
+  background-color: #F6F7F7;
+  position: absolute;
+  bottom: 0;
+  height: 40px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  color: #4B5C68
+}
+.navLinks{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.scheduleLink{
+  margin-right: 40px;
+}
+.footerItem{
+  padding-right : 40px;
+}
+
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+  name: String
+})
+console.log(props)
+
+const data = ref("Hello World1");
+const clickhandler = () =>{
+data.value = "now clicked1";
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="tableHeading">
+            <div class="headingItem">Date/Time</div>
+            <div class="headingItem">Stadium</div>
+            <div class="headingItem">Home Team</div>
+            <div class="headingItem">Away Team</div>
+    </div>
+    <div class="tableContent">
+      <div class="rowItem">5.5.2022</div>
+      <div class="rowItem">Marcana</div>
+      <div class="rowItem">
+          <div class="homeTeam">
+            <div class="teamName">Brazil</div>
+            <img width="53px" height="37px" src="https://flagsapi.codeaid.io/Brazil.png"/>
+            <div class="score">1:0</div>
+          </div>
+      </div>
+      <div class="rowItem">
+        <div class="awayTeam">
+            <img width="53px" height="37px" src="https://flagsapi.codeaid.io/Serbia.png"/>
+            <div class="teamName">Serbia</div>
+          </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>
+.container{
+  width: 90vw;
+  padding: 0 5vw;
+}
+.homeTeam{
+  display: flex;
+}
+.awayTeam{
+  display: flex;
+}
+.homeFlag{
+  background-image: url("https://flagsapi.codeaid.io/Brazil.png");
+  width: 53px;
+  height: 37px;
+}
+.tableHeading{
+  display: flex;
+  background-color: #E4EDF2;
+  height: 40px;
+  align-items: center;
+}
+.tableContent{
+  display: flex;
+  height: 70px;
+}
+.headingItem{
+  flex: 1;
+}
+.rowItem{
+  flex: 1;
+}
+</style>
